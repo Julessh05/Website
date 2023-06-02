3b5c0fdf5425362fa52448ff6c767b65e620363c
Julian Schumacher <professional@julianschumacher.dev>
2023-06-02 22:25:33 +0200

    Update Footer Divider

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,7 +4,9 @@ import styles from './footer.module.css'
 export default function Footer() {
     return (
         <div>
-            <hr />
+            <div className={styles.divider}>
+                <hr />
+            </div>
             <footer className={styles.footer}>
                 <p> © Julian Schumacher 2023 </p>
                 <p> All rights reserved </p>
