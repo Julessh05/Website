aab4a2fd43fbc33210190abd586a7ce52b7af4ed
Julian Schumacher <professional@julianschumacher.dev>
2025-12-28 13:41:16 +0100

    Add blog default page

---
+++ personal-website/src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+    return (
+        <main>
+            <h1>Blog</h1>
+            <p>Welcome to the blog page. Stay tuned for updates!</p>
+        </main>
+    );
+}
