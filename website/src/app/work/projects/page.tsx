eb9883bf2ad0606aa0d39a1be8cd2eb991206077
Julian Schumacher <professional@julianschumacher.dev>
2026-01-02 14:48:26 +0100

    Add "all" option on project page
    add games tab

--- website/src/app/work/projects/page.tsx
+++ website/src/app/work/projects/page.tsx
@@ -12,6 +12,10 @@ export default function Projects() {
     const ar: unknown[] = getProjects();
     const projects: Project[] = ar[0] as Project[];
     const types: ProjectType[] = ar[1] as ProjectType[];
+    const allType: ProjectType = {
+        identifier: "all",
+        name: "All"
+    }
     const [type, setType] = useState(types[0]);
 
     return (
@@ -21,6 +25,14 @@ export default function Projects() {
             <h2>Type</h2>
             <div className={styles.typeSelector}>
                 <ul>
+                    <li key="all">
+                        <button
+                            onClick={() => setType(allType)}
+                            className={"all" == type.identifier ? styles.active : ''}
+                        >
+                            All
+                        </button>
+                    </li>
                     {types.map((t) => (
                         <li key={t.identifier}>
                             <button
@@ -35,27 +47,76 @@ export default function Projects() {
             </div>
             {/* Projects */}
             <div style={{ height: "50px" }}></div>
-            <h2>Projects</h2>
-            <ul>
-                {projects
-                    .filter((p) => p.projectType == type.identifier)
-                    .map((project) => (
-                        <div key={project.identifier} className={styles.linkContainer}>
-                            <Link href={`projects/${project.identifier}`} className={styles.link}>
-                                <h4
-                                    className={styles.linkContent}
-                                >
-                                    {project.name}
-                                </h4>
-                                <p
-                                    className={styles.linkContent}
-                                >
-                                    {project.description}
-                                </p>
-                            </Link>
-                        </div>
-                    ))}
-            </ul>
+            {type.identifier == "all" ? ShowAllProjects(types, projects) :
+                <>
+                    <h2>{type.name}</h2>
+                    <ul>
+                        {projects.filter((p) => p.projectType == type.identifier).length == 0 ?
+                            <div>
+                                <b>No projects of this type available yet.</b>
+                                <p>Stay tuned for more!</p>
+                            </div> :
+                            projects
+                                .filter((p) => p.projectType == type.identifier)
+                                .map((project) => (
+                                    <div key={project.identifier} className={styles.linkContainer}>
+                                        <Link href={`projects/${project.identifier}`} className={styles.link}>
+                                            <h4
+                                                className={styles.linkContent}
+                                            >
+                                                {project.name}
+                                            </h4>
+                                            <p
+                                                className={styles.linkContent}
+                                            >
+                                                {project.description}
+                                            </p>
+                                        </Link>
+                                    </div>
+                                ))}
+                    </ul>
+                </>
+            }
         </main >
+    );
+}
+
+function ShowAllProjects(types: ProjectType[], projects: Project[]) {
+    return (
+        <div key={"all-projects"}>
+            {
+                types.map((type) => (
+                    <div key={`all-type-${type.identifier}`}>
+                        <h2>{type.name}</h2>
+                        <ul>
+                            {projects.filter((p) => p.projectType == type.identifier).length == 0 ?
+                                <div>
+                                    <b>No projects of this type available yet.</b>
+                                    <p>Stay tuned for more!</p>
+                                </div> :
+                                projects
+                                    .filter((p) => p.projectType == type.identifier)
+                                    .map((project) => (
+                                        <div key={`all-${project.identifier}`} className={styles.linkContainer}>
+                                            <Link href={`projects/${project.identifier}`} className={styles.link}>
+                                                <h4
+                                                    className={styles.linkContent}
+                                                >
+                                                    {project.name}
+                                                </h4>
+                                                <p
+                                                    className={styles.linkContent}
+                                                >
+                                                    {project.description}
+                                                </p>
+                                            </Link>
+                                        </div>
+                                    ))}
+                        </ul>
+                    </div>
+                )
+                )
+            }
+        </div>
     );
 }
